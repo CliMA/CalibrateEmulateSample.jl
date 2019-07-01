88750b8e3a45bfe3045ed4810b4ebdd418133ab7
Dima Burov <dburov.190@gmail.com>
2019-07-01 16:56:03 -0700

    Add ArgParse & ConfParser; read params from cfg.ini

--- examples/L96m/main.jl
+++ examples/L96m/main.jl
@@ -2,36 +2,117 @@
 
 import DifferentialEquations
 import PyPlot
+import ConfParser
+import ArgParse
+
 const DE = DifferentialEquations
 const plt = PyPlot
+const CP = ConfParser
+const AP = ArgParse
 include("L96m.jl")
 
+################################################################################
+# function section #############################################################
+################################################################################
+function parse_cli_args()
+  ap_settings = AP.ArgParseSettings()
+  AP.@add_arg_table ap_settings begin
+    "--cfg", "-c"
+      help = "name of the ini-file to be used as config"
+      arg_type = String
+      default = "cfg.ini"
+  end
+
+  return AP.parse_args(ap_settings)
+end
+
+function get_cfg_value(config::CP.ConfParse, sect::String, key::String, default)
+  return try
+    val = CP.retrieve(config, sect, key)
+    if isa(val, String) && String != typeof(default)
+      parse(typeof(default), val)
+    else
+      val
+    end
+  catch
+    default
+  end
+end
+
+function print_var(var_name::String; offset::Int = 0)
+  println(
+          " "^offset,
+          rpad(var_name, RPAD_VAR),
+          lpad(eval(Symbol(var_name)), LPAD_VAR)
+         )
+end
+
+function print_var(var_names::Array{String})
+  println("# Parameters")
+  for var_name in var_names
+    print_var(var_name, offset = 2)
+  end
+end
 
 ################################################################################
-# constants section ############################################################
+# config section ###############################################################
+################################################################################
+args_dict = parse_cli_args()
+config = try
+  CP.ConfParse(args_dict["cfg"])
+catch
+  CP.ConfParse("cfg.ini")
+end
+
+CP.parse_conf!(config)
+
+################################################################################
+# parameters section ###########################################################
 ################################################################################
 const RPAD = 42
+const RPAD_VAR = 21
 const LPAD_INTEGER = 7
 const LPAD_FLOAT = 13
-const SOLVER = DE.Tsit5()
+const LPAD_VAR = 29
 
-const T = 4 # integration time
-const T_compile = 1e-10 # force JIT compilation
-const T_conv = 100 # converging integration time
+parameters = String[]
+# integration parameters #######################################################
+# T            integration time
+# T_conv       converging integration time
+# T_compile    force JIT compilation
+push!(parameters, "T", "T_conv", "T_compile")
+
+const T = get_cfg_value(config, "integration", "T", 4.0)
+const T_conv = get_cfg_value(config, "integration", "T_conv", 100.0)
+const T_compile = 1e-10
 #const T_learn = 15 # time to gather training data for GP
 #const T_hist = 10000 # time to gather histogram statistics
 
-# integrator parameters
-const dtmax = 1e-3 # maximum step size
+# time-stepper parameters ######################################################
+# SOLVER       time-stepper itself
+# dtmax        maximum step size
+# reltol       relative tolerance
+# abstol       absolute tolerance
+push!(parameters, "SOLVER", "dtmax", "reltol", "abstol")
+
+const SOLVER_STR = get_cfg_value(config, "integration", "SOLVER", "Tsit5")
+const SOLVER = getfield(DE, Symbol(SOLVER_STR))()
+const dtmax = get_cfg_value(config, "integration", "dtmax", 1e-3)
 #const tau = 1e-3 # maximum step size for histogram statistics
 #const dt_conv = 0.01 # maximum step size for converging to attractor
-const reltol = 1e-3 # relative tolerance
-const abstol = 1e-6 # absolute tolerance
+const reltol = get_cfg_value(config, "integration", "reltol", 1e-3)
+const abstol = get_cfg_value(config, "integration", "abstol", 1e-6)
 
+# save/plot parameters #########################################################
 const k = 1 # index of the slow variable to save etc.
 const j = 1 # index of the fast variable to save/plot etc.
+push!(parameters, "k", "j")
 
+# L96 parameters ###############################################################
 const hx = -0.8
+push!(parameters, "hx")
+
+print_var(parameters)
 
 ################################################################################
 # IC section ###################################################################
@@ -50,6 +131,7 @@ end
 ################################################################################
 # main section #################################################################
 ################################################################################
+println("# Main")
 
 # force compilation of functions used in numerical integration
 print(rpad("(JIT compilation)", RPAD))
