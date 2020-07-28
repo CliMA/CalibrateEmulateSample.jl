a141e3b6b6f80ec688ad9c8a6d8e3df31d1541c7
Ignacio Lopez Gomez <ilopezgo@caltech.edu>
2020-07-28 15:30:47 -0700

    Modifications to PlottingUtils

--- src/PlottingUtils.jl
+++ src/PlottingUtils.jl
@@ -10,7 +10,13 @@ export eki_sphere_evol
 export plot_outputs
 export plot_eki_params
 
-function eki_sphere_evol(eki_u)
+"""
+    function eki_sphere_evol(eki_u)
+
+Returns the time evolution of the BoundingSphere parameters (center, radius)
+for the particle ensemble.
+"""
+function eki_sphere_evol(eki_u::Array{Array{Float64,2},1})
     #N-dimensional measures
     eki_center = zeros((length(eki_u), length(eki_u[1][1,:])))
     eki_radius = zeros(length(eki_u))
@@ -27,6 +33,12 @@ function eki_sphere_evol(eki_u)
     return eki_radius, eki_center_jump
 end
 
+"""
+    function plot_eki_params(eki_u)
+
+Plots the evolution of EK parameters with time. The ensemble is represented by 
+the mean, min and max value of each parameter at each timestep.
+"""
 function plot_eki_params(eki_u)
     #One-dimensional measures
     eki_mean = zeros( (length(eki_u), length(eki_u[1][1,:])) )
@@ -40,12 +52,6 @@ function plot_eki_params(eki_u)
     end
 
     for j in 1:length(eki_u[1][1,:])
-        p = plot((eki_u[1][:,j]), seriestype = :scatter)
-        for i in 2:length(eki_u)
-            plot!(p, eki_u[i][:,j], seriestype = :scatter)
-        end
-        savefig(string("EKI_points_param_",j,".png"))
-
         plot(eki_mean[:,j], ribbon=(
             -eki_min[:,j].+eki_mean[:,j], eki_max[:,j].-eki_mean[:,j]),
             label=string("parameter ", j), lw=2)
@@ -55,7 +61,37 @@ function plot_eki_params(eki_u)
     return
 end
 
+"""
+    function plot_outputs(eki_g, les_g, num_var, num_heights)
+
+Plots the mean output from the last EK ensemble. It is assumed that all 
+outputs are vertical profiles with the same number of vertical levels.
+"""
 function plot_outputs(eki_g, les_g, num_var, num_heights)
+    for sim in 1:length(num_var)
+        h_r = range(0, 1, length=Integer(num_heights[sim]))
+        for k in 1:num_var[sim]
+            inf_lim = Integer(
+                num_heights[sim]*(k-1) + (num_heights[1:sim-1]'*num_var[1:sim-1]))+1
+            sup_lim = Integer( inf_lim + num_heights[sim] - 1)
+            y_model_mean = mean(eki_g[end][:,inf_lim:sup_lim],dims=1)[1,:]
+
+            plot(les_g[inf_lim:sup_lim] , h_r, label="LES", lw=2)
+            plot!(y_model_mean, h_r, label="SCM", lw=2, ls=:dash)
+            ylabel!("Normalized height")
+            savefig(string("sim_",sim,"_output_variable",k,".png"))
+        end
+    end
+    return
+end
+
+"""
+    function plot_outputs(eki_g, les_g, num_var, num_heights)
+
+Plots the mean output from the last EK ensemble. It is assumed that all 
+outputs are vertical profiles with the same number of vertical levels.
+"""
+function plot_error_evolution(eki_g, les_g, num_var, num_heights)
     for sim in 1:length(num_var)
         h_r = range(0, 1, length=Integer(num_heights[sim]))
         for k in 1:num_var[sim]
