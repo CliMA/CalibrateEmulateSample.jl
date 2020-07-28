0caee6af270297941b6bbf4266a1dac8d41bc5c3
Ignacio Lopez Gomez <ilopezgo@caltech.edu>
2020-07-28 15:29:23 -0700

    Add test files

---
+++ src/SCModel.jl
@@ -0,0 +1,195 @@
+module SCModel
+
+using NCDatasets
+using Statistics
+using Interpolations
+
+export run_SCAMPy
+export obs_LES
+export get_profile
+
+function run_SCAMPy(u::Array{FT, 1},
+                    u_names::Array{String, 1},
+                    y_names::Union{Array{String, 1}, Array{Array{String,1},1}},
+                    scm_dir::String,
+                    ti::Union{FT, Array{FT,1}},
+                    tf::Union{FT, Array{FT,1}},
+                    ) where {FT<:AbstractFloat}
+    
+    exe_path = string(scm_dir, "call_SCAMPy.sh")
+    sim_uuid  = u[1]
+    for i in 2:length(u_names)
+        sim_uuid = string(sim_uuid,u[i])
+    end
+    command = `bash $exe_path $u $u_names`
+    run(command)
+
+    # SCAMPy file descriptor
+    sim_uuid = string(sim_uuid, ".txt")
+    sim_dirs = readlines(sim_uuid)
+    run(`rm $sim_uuid`)
+
+    y_scm = zeros(0)
+    for i in 1:length(sim_dirs)
+        sim_dir = sim_dirs[i]
+        if length(ti) > 1
+            ti_ = ti[i]
+            tf_ = tf[i]
+        else
+            ti_ = ti
+            tf_ = tf
+        end
+
+        if typeof(y_names)==Array{Array{String,1},1}
+            y_names_ = y_names[i]
+        else
+            y_names_ = y_names
+        end
+        append!(y_scm, get_profile(sim_dir, y_names_, ti = ti_, tf = tf_))
+        run(`rm -r $sim_dir`)
+    end
+
+    for i in eachindex(y_scm)
+        if isnan(y_scm[i])
+            y_scm[i] = 1.0e4
+        end
+    end
+
+    return y_scm
+end
+
+function obs_LES(y_names::Array{String, 1},
+                    sim_dir::String,
+                    ti::Float64,
+                    tf::Float64;
+                    z_scm::Union{Array{Float64, 1}, Nothing} = nothing,
+                    ) where {FT<:AbstractFloat}
+    
+    y_names_les = get_les_names(y_names, sim_dir)
+    y_highres = get_profile(sim_dir, y_names_les, ti = ti, tf = tf)
+    y_tvar_highres = get_timevar_profile(sim_dir, y_names_les, ti = ti, tf = tf)
+    if !isnothing(z_scm)
+        y_ = zeros(0)
+        y_tvar = zeros(0)
+        z_les = get_profile(sim_dir, ["z_half"])
+        num_outputs = Integer(length(y_highres)/length(z_les))
+        for i in 1:num_outputs
+            y_itp = interpolate( (z_les,), 
+                y_highres[1 + length(z_les)*(i-1) : i*length(z_les)],
+                Gridded(Linear()) )
+            append!(y_, y_itp(z_scm))
+
+            y_tvar_itp = interpolate( (z_les,), 
+                y_tvar_highres[1 + length(z_les)*(i-1) : i*length(z_les)],
+                Gridded(Linear()) )
+            append!(y_tvar, y_tvar_itp(z_scm))
+        end
+    else
+        y_ = y_highres
+        y_tvar = y_tvar_highres
+    end
+    return y_, y_tvar
+end
+
+function get_profile(sim_dir::String,
+                     var_name::Array{String,1};
+                     ti::Float64=0.0,
+                     tf::Float64=0.0,
+                     getFullHeights=false)
+
+    if length(var_name) == 1 && occursin("z_half", var_name[1])
+        prof_vec = nc_fetch(sim_dir, "profiles", var_name[1])
+    else
+        t = nc_fetch(sim_dir, "timeseries", "t")
+        dt = t[2]-t[1]
+        ti_diff, ti_index = findmin( broadcast(abs, t.-ti) )
+        tf_diff, tf_index = findmin( broadcast(abs, t.-tf) )
+        
+        prof_vec = zeros(0)
+        # If simulation does not contain values for ti or tf, return high value
+        if ti_diff > dt || tf_diff > dt
+            for i in 1:length(var_name)
+                var_ = nc_fetch(sim_dir, "profiles", var_name[i])
+                append!(prof_vec, 1.0e4*ones(length(var_[:, 1])))
+            end
+        else
+            for i in 1:length(var_name)
+                var_ = nc_fetch(sim_dir, "profiles", var_name[i])
+                # LES vertical fluxes are per volume, not mass
+                if occursin("resolved_z_flux", var_name[i])
+                    rho_half=nc_fetch(sim_dir, "reference", "rho0_half")
+                    var_ = var_.*rho_half
+                end
+                append!(prof_vec, mean(var_[:, ti_index:tf_index], dims=2))
+            end
+        end
+    end
+    return prof_vec 
+end
+
+function get_timevar_profile(sim_dir::String,
+                     var_name::Array{String,1};
+                     ti::Float64=0.0,
+                     tf::Float64=0.0,
+                     getFullHeights=false)
+
+    t = nc_fetch(sim_dir, "timeseries", "t")
+    dt = t[2]-t[1]
+    ti_diff, ti_index = findmin( broadcast(abs, t.-ti) )
+    tf_diff, tf_index = findmin( broadcast(abs, t.-tf) )
+    
+    prof_vec = zeros(0)
+    # If simulation does not contain values for ti or tf, return high value
+    for i in 1:length(var_name)
+        var_ = nc_fetch(sim_dir, "profiles", var_name[i])
+        # LES vertical fluxes are per volume, not mass
+        if occursin("resolved_z_flux", var_name[i])
+            rho_half=nc_fetch(sim_dir, "reference", "rho0_half")
+            var_ = var_.*rho_half
+        end
+        # append!(prof_vec, var(var_[:, ti_index:tf_index], dims=2) )
+        append!(prof_vec, maximum(var(var_[:, ti_index:tf_index], dims=2))
+            *ones(length(var_[:, 1]) ) )
+    end
+
+    return prof_vec
+end
+
+function get_les_names(scm_y_names::Array{String,1}, sim_dir::String)
+    y_names = deepcopy(scm_y_names)
+    if "thetal_mean" in y_names
+        if occursin("GABLS",sim_dir) || occursin("Soares",sim_dir)
+            y_names[findall(x->x=="thetal_mean", y_names)] .= "theta_mean"
+        else
+            y_names[findall(x->x=="thetal_mean", y_names)] .= "thetali_mean"
+        end
+    end
+    if "total_flux_qt" in y_names
+        y_names[findall(x->x=="total_flux_qt", y_names)] .= "resolved_z_flux_qt"
+    end
+    if "total_flux_h" in y_names && (occursin("GABLS",sim_dir) || occursin("Soares",sim_dir))
+        y_names[findall(x->x=="total_flux_h", y_names)] .= "resolved_z_flux_theta"
+    elseif "total_flux_h" in y_names
+        y_names[findall(x->x=="total_flux_h", y_names)] .= "resolved_z_flux_thetali"
+    end
+    if "u_mean" in y_names
+        y_names[findall(x->x=="u_mean", y_names)] .= "u_translational_mean"
+    end
+    if "v_mean" in y_names
+        y_names[findall(x->x=="v_mean", y_names)] .= "v_translational_mean"
+    end
+    if "tke_mean" in y_names
+        y_names[findall(x->x=="tke_mean", y_names)] .= "tke_nd_mean"
+    end
+    return y_names
+end
+
+function nc_fetch(dir, nc_group, var_name)
+    find_prev_to_name(x) = occursin("Output", x)
+    split_dir = split(dir, ".")
+    sim_name = split_dir[findall(find_prev_to_name, split_dir)[1]+1]
+    ds_group = NCDataset(string(dir, "/stats/Stats.", sim_name, ".nc")).group[nc_group]
+    return Array(ds_group[var_name])
+end
+
+end #module
---
+++ test/SCAMPy/runtests.jl
@@ -0,0 +1,344 @@
+# Import modules
+@everywhere using Pkg
+@everywhere Pkg.activate(".")
+@everywhere using Distributions  # probability distributions and associated functions
+@everywhere using StatsBase
+@everywhere using LinearAlgebra
+# Import Calibrate-Emulate-Sample modules
+@everywhere using CalibrateEmulateSample
+@everywhere using CalibrateEmulateSample.EKI
+@everywhere using CalibrateEmulateSample.GPEmulator
+@everywhere using CalibrateEmulateSample.MCMC
+@everywhere using CalibrateEmulateSample.Observations
+@everywhere using CalibrateEmulateSample.Utilities
+@everywhere using CalibrateEmulateSample.Transformations
+@everywhere using CalibrateEmulateSample.SCModel
+
+using GaussianProcesses
+using Plots
+using StatsPlots
+using JLD
+
+###
+###  Define the parameters and their priors
+###
+
+# Define the parameters that we want to learn
+
+@everywhere param_names = ["entrainment_factor", "detrainment_factor", "sorting_power", 
+	"tke_ed_coeff", "tke_diss_coeff", "pressure_normalmode_adv_coeff", 
+         "pressure_normalmode_buoy_coeff1", "pressure_normalmode_drag_coeff", "static_stab_coeff"]
+@everywhere param_names_symb = param_names
+@everywhere n_param = length(param_names)
+
+# Assume lognormal priors for all three parameters
+# Note: For the EDMF model to run, all parameters need to be nonnegative. 
+# The EKI update can result in violations of 
+# these constraints - therefore, we perform CES in log space, i.e.,
+# (the parameters can then simply be obtained by exponentiating the final results). 
+
+# Prior option 1: Log-normal in original space defined by mean and std
+@everywhere logmeans = zeros(n_param)
+@everywhere log_stds = zeros(n_param)
+@everywhere logmeans[1], log_stds[1] = logmean_and_logstd(0.2, 0.2)
+@everywhere logmeans[2], log_stds[2] = logmean_and_logstd(0.4, 0.2)
+@everywhere logmeans[3], log_stds[3] = logmean_and_logstd(2.0, 1.0)
+@everywhere logmeans[4], log_stds[4] = logmean_and_logstd(0.2, 0.2)
+@everywhere logmeans[5], log_stds[5] = logmean_and_logstd(0.2, 0.2)
+@everywhere logmeans[6], log_stds[6] = logmean_and_logstd(0.2, 0.2)
+@everywhere logmeans[7], log_stds[7] = logmean_and_logstd(0.2, 0.2)
+@everywhere logmeans[8], log_stds[8] = logmean_and_logstd(8.0, 1.0)
+@everywhere logmeans[9], log_stds[9] = logmean_and_logstd(0.2, 0.2)
+@everywhere priors = [Distributions.Normal(logmeans[1], log_stds[1]),
+                        Distributions.Normal(logmeans[2], log_stds[2]),
+                        Distributions.Normal(logmeans[3], log_stds[3]),
+                        Distributions.Normal(logmeans[4], log_stds[4]),
+                        Distributions.Normal(logmeans[5], log_stds[5]),
+                        Distributions.Normal(logmeans[6], log_stds[6]),
+                        Distributions.Normal(logmeans[7], log_stds[7]),
+                        Distributions.Normal(logmeans[8], log_stds[8]),
+                        Distributions.Normal(logmeans[9], log_stds[9])]
+
+# Second option: Uniform distributions
+#@everywhere priors = [Distributions.Uniform(0, 1),
+#                        Distributions.Uniform(0, 1),
+#                        Distributions.Uniform(0, 4),
+#                        Distributions.Uniform(0, 1),
+#                        Distributions.Uniform(0, 1),
+#                        Distributions.Uniform(0, 1),
+#                        Distributions.Uniform(0, 1),
+#                        Distributions.Uniform(3, 10),
+#                        Distributions.Uniform(0, 1) ]
+
+
+# # Prior option 2: Log-normal in original space defined by mode and std
+# logmean_c_m, logstd_c_m = logmean_and_logstd_from_mode_std(0.5, 0.3)
+# logmean_c_ϵ, logstd_c_ϵ = logmean_and_logstd_from_mode_std(0.5, 0.3)
+# println("Logmean and logstd of the prior: ", logmean_c_m, " ", logstd_c_m)
+# priors = [Distributions.Normal(logmean_c_m, logstd_c_m),    # prior on c_m
+#           Distributions.Normal(logmean_c_ϵ, logstd_c_ϵ)]    # prior on c_ϵ
+
+###
+###  Retrieve true LES samples
+###
+
+# This is the true value of the observables (e.g. LES ensemble mean for EDMF)
+@everywhere ti = [10800.0, 28800.0, 10800.0, 18000.0]
+@everywhere tf = [14400.0, 32400.0, 14400.0, 21600.0]
+@everywhere y_names = Array{String, 1}[]
+@everywhere push!(y_names, ["thetal_mean", "ql_mean", "qt_mean", "total_flux_h", "total_flux_qt"]) #DYCOMS_RF01
+@everywhere push!(y_names, ["thetal_mean", "u_mean", "v_mean", "tke_mean"]) #GABLS
+@everywhere push!(y_names, ["thetal_mean", "total_flux_h"]) #Nieuwstadt
+@everywhere push!(y_names, ["thetal_mean", "ql_mean", "qt_mean", "total_flux_h", "total_flux_qt"]) #Bomex
+
+# Get observations
+@everywhere yt = zeros(0)
+@everywhere yt_var = zeros(0)
+@everywhere sim_names = ["DYCOMS_RF01", "GABLS", "Nieuwstadt", "Bomex"]
+
+@everywhere les_dir = string("/groups/esm/ilopezgo/Output.", sim_names[1],".may20")
+@everywhere sim_dir = string("Output.", sim_names[1],".00000")
+@everywhere z_scm = get_profile(sim_dir, ["z_half"])
+@everywhere yt_, yt_var_ = obs_LES(y_names[1], les_dir, ti[1], tf[1], z_scm = z_scm)
+@everywhere append!(yt, yt_)
+@everywhere append!(yt_var, yt_var_)
+
+@everywhere les_dir = string("/groups/esm/ilopezgo/Output.", sim_names[2],".iles128wCov")
+@everywhere sim_dir = string("Output.", sim_names[2],".00000")
+@everywhere z_scm = get_profile(sim_dir, ["z_half"])
+@everywhere yt_, yt_var_ = obs_LES(y_names[2], les_dir, ti[2], tf[2], z_scm = z_scm)
+@everywhere append!(yt, yt_)
+@everywhere append!(yt_var, yt_var_)
+
+@everywhere les_dir = string("/groups/esm/ilopezgo/Output.Soares.dry11")
+@everywhere sim_dir = string("Output.", sim_names[3],".00000")
+@everywhere z_scm = get_profile(sim_dir, ["z_half"])
+@everywhere yt_, yt_var_ = obs_LES(y_names[3], les_dir, ti[3], tf[3], z_scm = z_scm)
+@everywhere append!(yt, yt_)
+@everywhere append!(yt_var, yt_var_)
+
+@everywhere les_dir = string("/groups/esm/ilopezgo/Output.Bomex.may18")
+@everywhere sim_dir = string("Output.", sim_names[4],".00000")
+@everywhere z_scm = get_profile(sim_dir, ["z_half"])
+@everywhere yt_, yt_var_ = obs_LES(y_names[4], les_dir, ti[4], tf[4], z_scm = z_scm)
+@everywhere append!(yt, yt_)
+@everywhere append!(yt_var, yt_var_)
+
+@everywhere n_observables = length(yt)
+
+# This is how many samples of the true data we have
+@everywhere n_samples = 1
+@everywhere samples = zeros(n_samples, length(yt))
+@everywhere samples[1,:] = yt
+# Noise level of the samples, which scales the time variance of each output.
+@everywhere noise_level = 1.0
+@everywhere Γy = noise_level^2 * convert(Array, Diagonal(yt_var))
+@everywhere μ_noise = zeros(length(yt))
+
+# We construct the observations object with the samples and the cov.
+@everywhere truth = Observations.Obs(samples, Γy, y_names[1])
+
+###
+###  Calibrate: Ensemble Kalman Inversion
+###
+
+@everywhere N_ens = 40 # number of ensemble members
+@everywhere N_iter =20 # number of EKI iterations.
+@everywhere N_yt = length(yt) # Length of data array
+
+@everywhere initial_params = EKI.construct_initial_ensemble(N_ens, priors)
+@everywhere ekiobj = EKI.EKIObj(initial_params, param_names, truth.mean, truth.obs_noise_cov)
+@everywhere g_ens = zeros(N_ens, n_observables)
+
+@everywhere scm_dir = "/home/ilopezgo/SCAMPy/"
+@everywhere params_i = deepcopy(exp_transform(ekiobj.u[end]))
+
+@everywhere g_(x::Array{Float64,1}) = run_SCAMPy(x, param_names,
+   y_names, scm_dir, ti, tf)
+
+outdir_path = string("results_p", n_param,"_n", noise_level,"_e", N_ens, "_i", N_iter, "_d", N_yt)
+command = `mkdir $outdir_path`
+try
+    run(command)
+catch e
+    println("Output directory already exists. Output may be overwritten.")
+end
+
+# EKI iterations
+@everywhere Δt = 1.0
+for i in 1:N_iter
+    # Note that the parameters are exp-transformed when used as input
+    # to SCAMPy
+    @everywhere params_i = deepcopy(exp_transform(ekiobj.u[end]))
+    @everywhere params_i = [params_i[i, :] for i in 1:size(params_i, 1)]
+    g_ens_arr = pmap(g_, params_i)
+    println("EKI iteration finished.")
+    for j in 1:N_ens
+      g_ens[j, :] = g_ens_arr[j]
+    end
+    EKI.update_ensemble!(ekiobj, g_ens; Δt=Δt)
+    println("\nEnsemble covariance det. for iteration ", size(ekiobj.u)[1])
+    println(det(cov(deepcopy((ekiobj.u[end])), dims=1)))
+    # Save EKI information to file
+    save( string(outdir_path,"/eki.jld"), "eki_u", ekiobj.u, "eki_g", ekiobj.g,
+        "truth_mean", ekiobj.g_t, "truth_cov", ekiobj.cov, "eki_err", ekiobj.err)
+end
+
+# EKI results: Has the ensemble collapsed toward the truth? Store and analyze.
+println("\nEKI ensemble mean at last stage:")
+println(mean(deepcopy(exp_transform(ekiobj.u[end])), dims=1))
+
+println("\nEnsemble covariance det. 1st EKI, transformed space.")
+println(det(cov(deepcopy((ekiobj.u[1])), dims=1)))
+
+println("\nEnsemble covariance det. last EKI, transformed space.")
+println(det(cov(deepcopy((ekiobj.u[end])), dims=1)))
+
+# Save EKI information to file
+save("eki.jld", "eki_u", ekiobj.u, "eki_g", ekiobj.g,
+        "truth_mean", ekiobj.g_t, "truth_cov", ekiobj.cov, "eki_err", ekiobj.err)
+
+###
+###  Emulate: Gaussian Process Regression
+###
+
+gppackage = GPEmulator.GPJL()
+pred_type = GPEmulator.YType()
+
+mean_train_error = []
+mean_test_error = []
+for i in 1:N_iter
+    # Get training and testing points:
+    u_train, g_train, u_test, g_test = Utilities.extract_GP_train_test(ekiobj, i, 0.9)
+
+    # Construct kernel:
+    rbf_len = log.(ones(size(u_train, 2)))
+    rbf_logstd = log(1.0)
+    rbf = SEArd(rbf_len, rbf_logstd)
+    # white noise for regularization. Right now it leads to convergence problems.
+    # white = Noise(log(1.0e-4))
+    # construct kernel
+    GPkernel =  rbf
+
+    normalized = false
+    global gpobj = GPEmulator.GPObj(u_train, g_train, gppackage; GPkernel=GPkernel,
+                             normalized=normalized, prediction_type=pred_type)
+
+    # Check how well the Gaussian Process regression predicts on the
+    # training and testing sets
+    y_train_sqbias = GPEmulator.gp_sqbias(gpobj, u_train, g_train)
+    println("Normalized mean training error:", mean(y_train_sqbias./yt_var) )
+    append!(mean_train_error, mean(y_train_sqbias./yt_var))
+    y_test_sqbias = GPEmulator.gp_sqbias(gpobj, u_test, g_test)
+    println("Normalized mean testing error:", mean(y_test_sqbias./yt_var) )
+    append!(mean_test_error, mean(y_test_sqbias./yt_var))
+
+end
+save("GP_errors.jld", "train_error", mean_train_error, "test_error", mean_test_error)
+
+
+###
+###  Sample: Markov Chain Monte Carlo
+###
+
+# initial values: use ensemble mean of last EKI iteration
+u0 = vec(mean(deepcopy(ekiobj.u[end]), dims=1))
+println("initial parameters for MCMC (log-transformed): ", u0)
+
+# MCMC parameters    
+mcmc_alg = "rwm" # random walk Metropolis
+
+# First let's run a short chain to determine a good step size
+burnin = 0
+step = 1.0e-1 # first guess
+max_iter = 5000
+yt_sample = truth.mean
+mcmc_test = MCMC.MCMCObj(yt_sample, truth.cov, 
+                         priors, step, u0, 
+                         max_iter, mcmc_alg, burnin)
+new_step = MCMC.find_mcmc_step!(mcmc_test, gpobj)
+
+# Now begin the actual MCMC
+println("Begin MCMC - with step size ", new_step)
+
+# reset parameters
+max_iter = 200000
+burnin = 50000
+
+# u0 has been modified by MCMCObj in the test, resetting.
+u0 = vec(mean(ekiobj.u[end], dims=1))
+
+mcmc = MCMC.MCMCObj(yt_sample, truth.obs_noise_cov, priors, 
+                    new_step, u0, max_iter, mcmc_alg, burnin)
+MCMC.sample_posterior!(mcmc, gpobj, max_iter)
+
+posterior = MCMC.get_posterior(mcmc)      
+
+post_mean = mean(posterior, dims=1)
+post_cov = cov(posterior, dims=1)
+println("posterior mean and variance for each parameter:")
+println(mean_and_std_from_ln(post_mean[1], post_cov[1,1]))
+println(mean_and_std_from_ln(post_mean[2], post_cov[2,2]))
+println(mean_and_std_from_ln(post_mean[3], post_cov[3,3]))
+
+# Save posterior to file
+save("posterior.jld", "posterior", posterior)
+
+# Plot the posteriors together with the priors and the 
+#   true parameter values in transformed space
+
+plot(posterior[:, 1], posterior[:, 2], seriestype = :scatter)
+savefig("MCMC_chain_transformed.png")
+
+for idx in 1:n_param
+    param = param_names_symb[idx]
+    
+    label = "true " * param
+    histogram(posterior[:, idx], bins=100, normed=true, fill=:slategray, 
+              lab="posterior", xlabel = param)
+    xs = collect(logmeans[idx]-3.0*log_stds[idx]:log_stds[idx]/20.0:logmeans[idx]+3.0*log_stds[idx])
+    plot!(xs, mcmc.prior[idx], w=2.6, color=:blue, lab="prior")
+
+    title!(param)
+    StatsPlots.savefig("posterior_"*param*".png")
+end
+
+# Plot the posteriors together with the priors and the 
+#   true parameter values in the original space
+
+plot(exp.(posterior[:, 1]), exp.(posterior[:, 2]), seriestype = :scatter, 
+    xlabel = param_names_symb[1], ylabel=param_names_symb[2])
+savefig("MCMC_chain_original.png")
+
+histogram2d(exp.(posterior[:, 1]), exp.(posterior[:, 2]), 
+    xlim = [0.0, 2.0], ylim = [0.0, 2.0], nbins=60, normed=true, 
+    xlabel = param_names_symb[1], ylabel=param_names_symb[2], c= :bilbao)
+savefig("Emulator_posterior_original.png")
+
+cornerplot(exp.(posterior), compact = true, label = param_names_symb, histpct = 0.5, color = :rainbow)
+savefig("cornerplot_original.png")
+
+corrplot(exp.(posterior), label = param_names_symb)
+savefig("corrplot_original.png")
+
+marginalhist(exp.(posterior[:, 1]), exp.(posterior[:, 2]), 
+    xlim = [0.0, 2.0], ylim = [0.0, 2.0], nbins=40, normed=true, 
+    xguide = param_names_symb[1], yguide=param_names_symb[2], c= :bilbao)
+savefig("marginalhist_original.png")
+
+for idx in 1:n_param
+    param = param_names_symb[idx]
+
+    label = "true " * param
+    histogram(exp.(posterior[:, idx]), bins=100, normed=true, fill=:slategray, 
+              lab="posterior", xlabel = param)
+    priors_ln = [Distributions.LogNormal(logmeans[1], log_stds[1]),
+                        Distributions.LogNormal(logmeans[2], log_stds[2]),
+                        Distributions.LogNormal(logmeans[3], log_stds[3])]
+
+    xs = collect(logmeans[idx]-3.0*log_stds[idx]:log_stds[idx]/20.0:logmeans[idx]+3.0*log_stds[idx])
+    plot!(exp.(xs), priors_ln[idx], w=2.6, color=:blue, lab="prior")
+
+    title!(param)
+    StatsPlots.savefig("posterior_orig_space_"*param*".png")
+end
