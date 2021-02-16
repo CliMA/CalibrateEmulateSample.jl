f87ab278aa2a41fa646e6214ad28427c818ef0bb
ilopezgp <ilopezgp@gmail.com>
2021-02-16 15:09:52 -0800

    WIP: Calibration step

---
+++ test/ClimateMachine/sstep_calibration.jl
@@ -0,0 +1,34 @@
+using Distributions
+using JLD
+using ArgParse
+# Import Calibrate-Emulate-Sample modules
+using CalibrateEmulateSample.Priors
+using CalibrateEmulateSample.EKP
+using CalibrateEmulateSample.Observations
+using CalibrateEmulateSample.Utilities
+
+s = ArgParseSettings()
+@add_arg_table s begin
+    "--iteration"
+        help = "Calibration iteration number"
+        arg_type = Int
+        default = 1
+end
+parsed_args = parse_args(ARGS, s)
+iteration_ = parsed_args["iteration"]
+
+# Recover inputs
+versions = readlines("versions_$(iteration_).txt")
+open("$(versions[1]).output/$(versions[1])", "r") do io
+    n_params = length([float(line) for (index, line) in enumerate(eachline(io)) if index%3 == 0])
+end
+u = zeros(length(versions), n_params)
+for (ens_index, version_) in enumerate(versions)
+    open("$(version_).output/$(version_)", "r") do io
+        u[ens_index, :] = [float(line) for (index, line) in enumerate(eachline(io)) if index%3 == 0]
+    end
+end
+
+open("recovered_params_$(iteration_).txt", "w") do io
+    write(io, u)
+end
