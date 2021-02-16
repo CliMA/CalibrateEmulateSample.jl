5c6d4eb8a7429c0c174a48b69707e5338c83840e
ilopezgp <ilopezgp@gmail.com>
2021-02-16 15:32:40 -0800

    Bug fix

--- test/ClimateMachine/sstep_calibration.jl
+++ test/ClimateMachine/sstep_calibration.jl
@@ -7,6 +7,25 @@ using CalibrateEmulateSample.EKP
 using CalibrateEmulateSample.Observations
 using CalibrateEmulateSample.Utilities
 
+
+function reconstruct_ek_obj(iteration_)
+    # Construct EK object
+    versions = readlines("versions_$(iteration_).txt")
+    open("$(versions[1]).output/$(versions[1])", "r") do io
+        n_params = length([parse(Float64, line) for (index, line) in enumerate(eachline(io)) if index%3 == 0])
+        u_names = [split(line, "(")[1] for (index, line) in enumerate(eachline(io)) if index%2 == 0]
+    end
+    u = zeros(length(versions), n_params)
+    for (ens_index, version_) in enumerate(versions)
+        open("$(version_).output/$(version_)", "r") do io
+            u[ens_index, :] = [parse(Float64, line) for (index, line) in enumerate(eachline(io)) if index%3 == 0]
+        end
+    end
+    open("recovered_params_$(iteration_).txt", "w") do io
+        write(io, u)
+    end
+end
+
 s = ArgParseSettings()
 @add_arg_table s begin
     "--iteration"
@@ -16,20 +35,4 @@ s = ArgParseSettings()
 end
 parsed_args = parse_args(ARGS, s)
 iteration_ = parsed_args["iteration"]
-
-# Construct EK object
-versions = readlines("versions_$(iteration_).txt")
-open("$(versions[1]).output/$(versions[1])", "r") do io
-    n_params = length([parse(Float64, line) for (index, line) in enumerate(eachline(io)) if index%3 == 0])
-    u_names = [split(line, "(")[1] for (index, line) in enumerate(eachline(io)) if index%2 == 0]
-end
-u = zeros(length(versions), n_params)
-for (ens_index, version_) in enumerate(versions)
-    open("$(version_).output/$(version_)", "r") do io
-        u[ens_index, :] = [parse(Float64, line) for (index, line) in enumerate(eachline(io)) if index%3 == 0]
-    end
-end
-
-open("recovered_params_$(iteration_).txt", "w") do io
-    write(io, u)
-end
+reconstruct_ek_obj(iteration_)
