b1221b2b5ce793912ad34d26be753884babfcd38
odunbar <odunbar@caltech.edu>
2022-05-26 10:44:24 -0700

    climaformat

--- examples/Lorenz/Lorenz_example.jl
+++ examples/Lorenz/Lorenz_example.jl
@@ -109,7 +109,7 @@ if dynamics == 2
     #prior_means = [F_true+0.5, A_true+0.5]
     prior_means = [F_true, A_true]
     prior_stds = [3.0, 0.5 * A_true]
-    
+
     prior_F = Dict(
         "distribution" => Parameterized(Normal(prior_means[1], prior_stds[1])),
         "constraint" => no_constraint(),
@@ -120,14 +120,10 @@ if dynamics == 2
         "constraint" => no_constraint(),
         "name" => param_names[2],
     )
-    
+
     priors = ParameterDistribution([prior_F, prior_A])
 else
-    prior_F = Dict(
-        "distribution" => Parameterized(Normal(F_true, 1)),
-        "constraint" => no_constraint(),
-        "name" => "F",
-    )
+    prior_F = Dict("distribution" => Parameterized(Normal(F_true, 1)), "constraint" => no_constraint(), "name" => "F")
     priors = ParameterDistribution(prior_F)
 end
 
