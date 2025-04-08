f5dd724d3f62f761424641b1d9ac2fa669ba0f4c
odunbar <odunbar@caltech.edu>
2025-04-08 11:24:57 -0700

    Fixing multithreading with chunks
    typo
    
    scale priors with dimension, and duplicate buffers
    
    rm info statements
    
    rm prior scalings for now
    
    formatting
    
    format
    
    format again
    
    rm maxthreadid

--- examples/Lorenz/GModel.jl
+++ examples/Lorenz/GModel.jl
@@ -4,17 +4,22 @@ export run_G
 export run_G_ensemble
 export lorenz_forward
 
+using ChunkSplitter
+
 include("GModel_common.jl")
 
 function run_ensembles(settings, lorenz_params, nd, N_ens)
     nthreads = Threads.nthreads()
-    g_ens = zeros(nthreads, nd, N_ens)
-    Threads.@threads for i in 1:N_ens
-        tid = Threads.threadid()
-        # run the model with the current parameters, i.e., map θ to G(θ)
-        g_ens[tid, :, i] = lorenz_forward(settings, lorenz_params[i])
+
+    chunked_ensemble = chunks(1:N_ens, n = nthreads) # could probs do without this package. But gives (tid, idx for tid)
+
+    g_ens = zeros(nd, N_ens)
+    Threads.@threads for e_idx in chunked_ensemble
+        for i in e_idx
+            # run the model with the current parameters, i.e., map θ to G(θ)
+            g_ens[:, i] = lorenz_forward(settings, lorenz_params[i])
+        end
     end
-    g_ens = dropdims(sum(g_ens, dims = 1), dims = 1)
     return g_ens
 end
 
