ab204d0500c0f6db034973c817f69a07d95538f0
odunbar <odunbar@caltech.edu>
2025-06-26 15:01:16 -0700

    remove old normalization/standardizations code

--- src/Emulator.jl
+++ src/Emulator.jl
@@ -184,19 +184,31 @@ function predict(
     encoded_outputs, encoded_uncertainties = predict(get_machine_learning_tool(emulator), get_data(encoded_inputs), mlt_kwargs...)
 
     var_or_cov = (ndims(encoded_uncertainties) == 2) ? "var" : "cov"
-    
+
     # return decoded or encoded?
     if transform_to_real
         decoded_outputs = decode_with_schedule(encoder_schedule, DataContainer(encoded_outputs), "out")
-        
-        decoded_covariances = (var_or_cov == "var") ?
-            [decode_with_schedule(encoder_schedule, Diagonal(col), "out") for col in eachcol(encoded_uncertainties) ] :
-            [decode_with_schedule(encoder_schedule, mat, "out") for mat in eachslice(encoded_uncertainties, dims=3) ] 
-        return get_data(decoded_outputs), decoded_covariances
+   
+        decoded_covariances = zeros(output_dim, output_dim, size(encoded_uncertainties)[end])
+        if var_or_cov == "var"
+            for (i,col) in enumerate(eachcol(encoded_uncertainties))
+               decoded_covariances[:,:,i] .= decode_with_schedule(encoder_schedule, Diagonal(col), "out")
+            end
+        else # == "cov"
+            for (i,mat) in enumerate(eachslice(encoded_uncertainties, dims=3))
+               decoded_covariances[:,:,i] .= decode_with_schedule(encoder_schedule, mat, "out")
+            end
+        end
+            
+        return get_data(decoded_outputs), eachslice(decoded_covariances,dims=3)
     else
         
         if encoded_output_dim > 1
-            encoded_covariances = [Diagonal(col) for col in eachcol(encoded_uncertainties)]
+            encoded_covariances_mat = zeros(output_dim, output_dim, size(encoded_uncertainties)[end])
+            for (i,col) in enumerate(eachcol(encoded_uncertainties))
+                encoded_covariances_mat[:,:,i] = Diagonal(col)
+            end
+            encoded_covariances = eachslice(encoded_covariances_mat,dims=3)
         else
             encoded_covariances = encoded_uncertainties
         end
@@ -204,255 +216,6 @@ function predict(
     end
     
 end
-
-#=
-
-# Normalization, Standardization, and Decorrelation
-"""
-$(DocStringExtensions.TYPEDSIGNATURES)
-
-Calculate the normalization of inputs.
-"""
-function calculate_normalization(inputs::VOrM) where {VOrM <: AbstractVecOrMat}
-    input_mean = vec(mean(inputs, dims = 2))
-    input_cov = cov(inputs, dims = 2)
-
-    if rank(input_cov) == size(input_cov, 1)
-        normalization = sqrt(inv(input_cov))
-    else # if not full rank, normalize non-zero singular values
-        svd_in = svd(input_cov)
-        sqrt_inv_sv = 1 ./ sqrt.(svd_in.S[1:rank(input_cov)])
-        normalization = Diagonal(sqrt_inv_sv) * svd_in.Vt[1:rank(input_cov), :] #non-square
-        @info "reducing input dimension from $(size(input_cov,1)) to $rank(input_cov) during low rank in normalization"
-    end
-    return normalization
-end
-
-"""
-$(DocStringExtensions.TYPEDSIGNATURES)
-
-Normalize the input data, with a normalizing function.
-"""
-function normalize(emulator::Emulator, inputs::VOrM) where {VOrM <: AbstractVecOrMat}
-    if emulator.normalize_inputs
-        return normalize(inputs, emulator.input_mean, emulator.normalization)
-    else
-        return inputs
-    end
-end
-
-function normalize(
-    inputs::VOrM,
-    input_mean::V,
-    normalization::M,
-) where {VOrM <: AbstractVecOrMat, V <: AbstractVector, M <: AbstractMatrix}
-    return normalization * (inputs .- input_mean)
-end
-"""
-$(DocStringExtensions.TYPEDSIGNATURES)
-
-Standardize with a vector of factors (size equal to output dimension).
-"""
-function standardize(
-    outputs::VorM,
-    output_covs::VofMUS,
-    factors::V,
-) where {
-    VorM <: AbstractVecOrMat,
-    VofMUS <: Union{AbstractVector{<:AbstractMatrix}, AbstractVector{<:UniformScaling}},
-    V <: AbstractVector,
-}
-    # Case where `output_covs` is a Vector of covariance matrices
-    n = length(factors)
-    outputs = outputs ./ factors
-    cov_factors = factors .* factors'
-    for i in 1:length(output_covs)
-        if isa(output_covs[i], Matrix)
-            output_covs[i] = output_covs[i] ./ cov_factors
-        elseif isa(output_covs[i], UniformScaling)
-            # assert all elements of factors are same, otherwise can't cast back to
-            # UniformScaling
-            @assert all(y -> y == first(factors), factors)
-            output_covs[i] = output_covs[i] / cov_factors[1, 1]
-        else
-            # Diagonal, TriDiagonal etc. case
-            # need to do ./ as dense matrix and cast back to original type
-            # https://discourse.julialang.org/t/get-generic-constructor-of-parametric-type/57189/8
-            T = typeof(output_covs[i])
-            cast_T = getfield(parentmodule(T), nameof(T))
-            output_covs[i] = cast_T(Matrix(output_covs[i]) ./ cov_factors)
-        end
-    end
-    return outputs, output_covs
-end
-
-function standardize(
-    outputs::VorM,
-    output_cov::MorVorUS,
-    factors::V,
-) where {
-    VorM <: AbstractVecOrMat,
-    MorVorUS <: Union{AbstractMatrix, AbstractVector{<:AbstractFloat}, UniformScaling}, # must be vector of floats or could cause inf. loop with other method definition.
-    V <: AbstractVector,
-}
-
-    # Case where `output_cov` is a single covariance matrix
-    stdized_out, stdized_covs = standardize(outputs, [output_cov], factors)
-    return stdized_out, stdized_covs[1]
-end
-
-"""
-$(DocStringExtensions.TYPEDSIGNATURES)
-
-Reverse a previous standardization with the stored vector of factors (size equal to output 
-dimension). `output_cov` is a Vector of covariance matrices, such as is returned by
-[`svd_reverse_transform_mean_cov`](@ref).
-"""
-function reverse_standardize(
-    emulator::Emulator{FT},
-    outputs::VorM,
-    output_covs::VorMtwo,
-) where {
-    VorM <: AbstractVecOrMat,
-    VorMtwo <: AbstractVecOrMat, # can be different type
-    FT <: AbstractFloat,
-}
-    if emulator.standardize_outputs
-        return standardize(outputs, output_covs, 1.0 ./ emulator.standardize_outputs_factors)
-    else
-        return outputs, output_covs
-    end
-end
-
-
-
-"""
-$(DocStringExtensions.TYPEDSIGNATURES)
-
-Apply a singular value decomposition (SVD) to the data
-  - `data` - GP training data/targets; size *output\\_dim* × *N\\_samples*
-  - `obs_noise_cov` - covariance of observational noise
-  - `truncate_svd` - Project onto this fraction of the largest principal components. Defaults
-    to 1.0 (no truncation).
-
-Returns the transformed data and the decomposition, which is a matrix 
-factorization of type LinearAlgebra.SVD. 
-  
-Note: If `F::SVD` is the factorization object, U, S, V and Vt can be obtained via 
-F.U, F.S, F.V and F.Vt, such that A = U * Diagonal(S) * Vt. The singular values 
-in S are sorted in descending order.
-"""
-function svd_transform(
-    data::AbstractMatrix{FT},
-    obs_noise_cov::Union{AbstractMatrix{FT}, Nothing};
-    retained_svd_frac::FT = 1.0,
-) where {FT <: AbstractFloat}
-    if obs_noise_cov === nothing
-        # no-op case
-        return data, nothing
-    end
-    # actually have a matrix to take the SVD of
-    decomp = svd(obs_noise_cov)
-    sqrt_singular_values_inv = Diagonal(1.0 ./ sqrt.(decomp.S))
-    if retained_svd_frac < 1.0
-        # Truncate the SVD as a form of regularization
-        # Find cutoff
-        S_cumsum = cumsum(decomp.S) / sum(decomp.S)
-        ind = findall(x -> (x > retained_svd_frac), S_cumsum)
-        k = ind[1]
-        n = size(data)[1]
-        println("SVD truncated at k: ", k, "/", n)
-        # Apply truncated SVD
-        transformed_data = sqrt_singular_values_inv[1:k, 1:k] * decomp.Vt[1:k, :] * data
-        decomposition = SVD(decomp.U[:, 1:k], decomp.S[1:k], decomp.Vt[1:k, :])
-    else
-        transformed_data = sqrt_singular_values_inv * decomp.Vt * data
-        decomposition = decomp
-    end
-    return transformed_data, decomposition
-end
-
-function svd_transform(
-    data::AbstractVector{FT},
-    obs_noise_cov::Union{AbstractMatrix{FT}, Nothing};
-    retained_svd_frac::FT = 1.0,
-) where {FT <: AbstractFloat}
-    # method for 1D data
-    transformed_data, decomposition =
-        svd_transform(reshape(data, :, 1), obs_noise_cov; retained_svd_frac = retained_svd_frac)
-    return vec(transformed_data), decomposition
-end
-
-function svd_transform(
-    data::AbstractVecOrMat{FT},
-    obs_noise_cov::UniformScaling{FT};
-    retained_svd_frac::FT = 1.0,
-) where {FT <: AbstractFloat}
-    # method for UniformScaling
-    return svd_transform(data, Diagonal(obs_noise_cov, size(data, 1)); retained_svd_frac = retained_svd_frac)
-end
-
-"""
-$(DocStringExtensions.TYPEDSIGNATURES)
-
-Transform the mean and covariance back to the original (correlated) coordinate system
-  - `μ` - predicted mean; size *output\\_dim* × *N\\_predicted\\_points*.
-  - `σ2` - predicted variance; size *output\\_dim* × *N\\_predicted\\_points*.
-         - predicted covariance; size *N\\_predicted\\_points* array of size *output\\_dim* × *output\\_dim*.
-  - `decomposition` - SVD decomposition of *obs\\_noise\\_cov*.
-
-Returns the transformed mean (size *output\\_dim* × *N\\_predicted\\_points*) and variance. 
-Note that transforming the variance back to the original coordinate system
-results in non-zero off-diagonal elements, so instead of just returning the 
-elements on the main diagonal (i.e., the variances), we return the full 
-covariance at each point, as a vector of length *N\\_predicted\\_points*, where 
-each element is a matrix of size *output\\_dim* × *output\\_dim*.
-"""
-function svd_reverse_transform_mean_cov(
-    μ::AbstractMatrix{FT},
-    σ2::AbstractVector,# vector of output_dim x output_dim matrices
-    decomposition::SVD,
-) where {FT <: AbstractFloat}
-
-    N_predicted_points = length(σ2)
-    if !(eltype(σ2) <: AbstractMatrix)
-        throw(
-            ArgumentError(
-                "input σ2 must be a vector of eltype <: AbstractMatrix, instead  eltype(σ2) = ",
-                string(eltype(σ2)),
-            ),
-        )
-    end
-
-    # We created meanvGP = D_inv * Vt * mean_v so meanv = V * D * meanvGP
-    sqrt_singular_values = Diagonal(sqrt.(decomposition.S))
-    transformed_μ = decomposition.V * sqrt_singular_values * μ
-
-    transformed_σ2 = Vector{Symmetric{FT, Matrix{FT}}}(undef, N_predicted_points)
-    # Back transformation
-
-    for j in 1:N_predicted_points
-        σ2_j = decomposition.V * sqrt_singular_values * σ2[j] * sqrt_singular_values * decomposition.Vt
-        transformed_σ2[j] = Symmetric(σ2_j)
-    end
-
-    return transformed_μ, transformed_σ2
-
-end
-
-function svd_reverse_transform_mean_cov(
-    μ::AbstractMatrix{FT},
-    σ2::AbstractMatrix{FT},
-    decomposition::SVD,
-) where {FT <: AbstractFloat}
-    @assert size(μ) == size(σ2)
-    output_dim, N_predicted_points = size(σ2)
-    σ2_as_cov = vec([Diagonal(σ2[:, j]) for j in 1:N_predicted_points])
-
-    return svd_reverse_transform_mean_cov(μ, σ2_as_cov, decomposition)
-end
-=#
-
 
 
 end
