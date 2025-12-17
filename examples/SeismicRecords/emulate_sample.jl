3d66b1e6a5a5146bb4a0c9d9db60c1590194a7e8
odunbar <odunbar@caltech.edu>
2025-12-17 10:22:57 -0800

    copy script into output files

--- examples/SeismicRecords/emulate_sample.jl
+++ examples/SeismicRecords/emulate_sample.jl
@@ -27,7 +27,10 @@ out_dir= joinpath(@__DIR__,"output_$(case)")
 if !isdir(out_dir)
     mkdir(out_dir)
 end
+# copy this script into the output directory
+cp(abspath(@__FILE__), joinpath(out_dir, basename(@__FILE__)); force=true)
 @info "Running case $case, \n outputs will be stored in $out_dir"
+
 # get the priors
 priors_filename = "priors_experiment3_0.toml"
 param_dict = TOML.parsefile(priors_filename)
