6341ca240aa24d3b28c1e16abeefa94756c9db51
Oliver Dunbar <47412152+odunbar@users.noreply.github.com>
2026-04-24 16:28:09 -0700

    change env variable in GHA and add test message to help keep it consistent (#417)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,15 @@
 using Test
 
-TEST_PLOT_OUTPUT = !isempty(get(ENV, "CES_TEST_PLOT_OUTPUT", ""))
+TEST_PLOT_OUTPUT = get(ENV, "CES_TEST_PLOT_OUTPUT", false)
+@info "[in test/runtest.jl], create plots? CES_TEST_PLOT_OUTPUT: $(TEST_PLOT_OUTPUT)"
 
 if TEST_PLOT_OUTPUT
     using Plots
 end
 
+ENV["SKLEARN_JL_VERSION"] = "1.5.1"
+sklearn_jl_version = get(ENV, "SKLEARN_JL_VERSION", "1.8.0")
+@info "[in test/runtest.jl], (if running Github actions) this variable must match the scikit learn version in .github/workflows/tests.yml. SKLEARN_JL_VERSION: $(sklearn_jl_version)"
 
 function include_test(_module)
     println("Starting tests for $_module")
